f004b97136641c575e08272dbcec14490c828b6a
Zasetsky <zasetsky.evgeny@gmail.com>
2022-04-05 20:43:01 +0300

    cities and online checkbox

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <FormPage />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FormPage from './components/FormPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FormPage
   }
 }
 </script>
